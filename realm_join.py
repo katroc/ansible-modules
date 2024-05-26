6941503b9d1a006c7d6c95d3bae9f806364a9805
kactroc <ansible@terraform.tronlab.dev>
2024-05-27 00:40:54 +1000

    added state to realm_join

--- realm_join.py
+++ realm_join.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 
 import subprocess
+import re
 from ansible.module_utils.basic import AnsibleModule
 
 def run_command(command, input_data=None):
     try:
         result = subprocess.run(command, input=input_data, text=True, capture_output=True, check=True)
-        stdout = result.stdout.replace("Password for", "")  # Filter out password prompts
+        stdout = filter_password_prompts(result.stdout)
         stderr = result.stderr
         return stdout, stderr, result.returncode
     except subprocess.CalledProcessError as e:
-        return e.stdout, e.stderr, e.returncode
+        return filter_password_prompts(e.stdout), e.stderr, e.returncode
+
+def filter_password_prompts(output):
+    # Use a regex to filter out any line that starts with "Password for"
+    return re.sub(r'^Password for .+: $', '', output, flags=re.MULTILINE)
 
 def parse_realm_details(realm_details_str):
     realm_details_dict = {}
@@ -36,7 +41,8 @@ def main():
             user=dict(type='str', required=True),
             password=dict(type='str', required=True, no_log=True),
             computer_ou=dict(type='str', required=False),
-            manage_sssd=dict(type='bool', default=False)
+            manage_sssd=dict(type='bool', default=False),
+            state=dict(type='str', choices=['present', 'absent'], default='present')
         ),
         supports_check_mode=True
     )
@@ -46,30 +52,44 @@ def main():
     password = module.params['password']
     computer_ou = module.params.get('computer_ou')
     manage_sssd = module.params['manage_sssd']
+    state = module.params['state']
 
     if module.check_mode:
         module.exit_json(changed=True)
 
-    cmd = ['realm', 'join', '--user', user, domain]
-    if computer_ou:
-        cmd.extend(['--computer-ou', computer_ou])
-    if manage_sssd:
-        cmd.append('--automatic-id-mapping=no')
+    if state == 'present':
+        cmd = ['realm', 'join', '--user', user, domain]
+        if computer_ou:
+            cmd.extend(['--computer-ou', computer_ou])
+        if manage_sssd:
+            cmd.append('--automatic-id-mapping=no')
 
-    stdout, stderr, rc = run_command(cmd, input_data=password)
+        stdout, stderr, rc = run_command(cmd, input_data=password)
 
-    if rc != 0:
-        if "Already joined to this domain" in stderr:
-            module.exit_json(changed=False, msg=f"System is already joined to {domain}", stdout=stdout, stderr="")
-        else:
-            module.fail_json(msg="Failed to join realm", stderr=stderr, stdout=stdout, rc=rc)
+        if rc != 0:
+            if "Already joined to this domain" in stderr:
+                module.exit_json(changed=False, msg=f"System is already joined to {domain}", stdout=stdout, stderr="")
+            else:
+                module.fail_json(msg="Failed to join realm", stderr=stderr, stdout=stdout, rc=rc)
 
-    realm_details_str, stderr, rc = run_command(['realm', 'list'])
-    if rc != 0:
-        module.fail_json(msg="Failed to retrieve realm details after join", stderr=stderr, stdout=realm_details_str, rc=rc)
+        realm_details_str, stderr, rc = run_command(['realm', 'list'])
+        if rc != 0:
+            module.fail_json(msg="Failed to retrieve realm details after join", stderr=stderr, stdout=realm_details_str, rc=rc)
 
-    realm = parse_realm_details(realm_details_str)
-    module.exit_json(changed=True, msg=f"Successfully joined to {domain}", stdout=stdout, stderr=stderr, realm=realm)
+        realm = parse_realm_details(realm_details_str)
+        module.exit_json(changed=True, msg=f"Successfully joined to {domain}", stdout=stdout, stderr=stderr, realm=realm)
+
+    elif state == 'absent':
+        cmd = ['realm', 'leave', domain]
+        stdout, stderr, rc = run_command(cmd, input_data=password)
+
+        if rc != 0:
+            if "Not joined to this domain" in stderr:
+                module.exit_json(changed=False, msg=f"System is not joined to {domain}", stdout=stdout, stderr="")
+            else:
+                module.fail_json(msg="Failed to leave realm", stderr=stderr, stdout=stdout, rc=rc)
+
+        module.exit_json(changed=True, msg=f"Successfully left {domain}", stdout=stdout, stderr=stderr)
 
 if __name__ == '__main__':
     main()
